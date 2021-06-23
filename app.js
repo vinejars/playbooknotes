2d88d0961999239b4b392928e0cfc250af01b988
Em Comeau <comeau.amanda.n@gmail.com>
2021-06-23 19:16:56 -0400

    feat: can edit, delete, and switch notes

--- app.js
+++ app.js
@@ -51,21 +51,59 @@ if(noteArray.length < 2 && !noteArray.includes(currentNote)){
     console.log('spliced array: ', noteArray)
 } else if(noteArray.length === 2){
     noteArray = [];
-    alert('You cannot select more than 2 notes at a time!')
+    alert(`You can't select more than 2 notes at a time!`)
 }
 currentNote = null;
 }
 
 const addNote = function(){
-    if(noteArray.length === 2){
+    if(noteArray.length === 2 && !noteArray[1].innerText){
     noteArray[1].innerText = document.querySelector("input").value
     document.querySelector("input").value = ''
-    } else {
+    } else if (!noteArray[0].innerText && noteArray.length !== 2){
     noteArray[0].innerText = document.querySelector("input").value
     document.querySelector("input").value = ''
-
+    }
+    else{
+        alert(`You can't add a note to a pre-existing note!`)
     }
 }
+
+const switchNotes = function(){
+    if(noteArray.length === 2){
+        let text1;
+        let text2;
+        text1 = noteArray[0].innerText;
+        text2 = noteArray[1].innerText;
+        noteArray[1].innerText = text1;
+        noteArray[0].innerText = text2;
+
+    } else {
+        alert(`You've gotta click 2 notes to switch 'em!`)
+    }
+}
+
+const deleteNote = function(){
+    if(noteArray.length === 2){
+    noteArray[1].innerText = null;
+    } else{
+    noteArray[0].innerText = null;
+    }
+}
+
+const editNote = function(){
+    if(noteArray.length === 2 && noteArray[1].innerText){
+       let newText = prompt('Please edit note below:', `${noteArray[1].innerText}`);
+       console.log(newText)
+       noteArray[1].innerText = newText;
+        } else if (noteArray.length !== 2 && noteArray[0].innerText){
+         let newText = prompt('Please edit note below:', `${noteArray[0].innerText}`)
+         console.log(newText)
+          noteArray[0].innerText = newText;
+        } else {
+            alert(`There needs to be text to edit!`)
+        }
+    }
 
 
 
